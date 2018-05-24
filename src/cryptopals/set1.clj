4aafdd8f87bff5580c44a0111fbcad19dce4edae
Philip Hale <philip@pghale.com>
2018-05-24 21:40:10 +0100

    (hex-to-bytes) fn

--- src/cryptopals/set1.clj
+++ src/cryptopals/set1.clj
@@ -1,10 +1,32 @@
 (ns cryptopals.set1)
 
-;; Always operate on raw bytes, never on encoded strings. Only use hex and base64 for pretty-printing.
+;; RULE: Always operate on raw bytes, never on encoded strings. Only use hex and base64 for pretty-printing.
 
-(defn hex-to-base64
-  "Challenge 1: Convert hex to base64"
-  [hex]
-  (str "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"))
+;; Step 1: the input is a string, we must convert this to an array of bytes
+;;  1a. Split the string into chunks of two, as each HEX value is a pair of chars
+;;  1b. Interpret the two-char string as a hex
+(defn hex-to-bytes
+  "UNSAFE -- interprets string as hex using read-string"
+  [hex] (let [chars (partition 2 hex)
+              parse-chars (fn [[c1 c2]] (read-string (str "0x" c1 c2)))]
+          (map parse-chars chars)))
 
-;; TODO: write implementation, return the raw bytes not a string
+(defn print-bytes
+  "[dev] Turn byte arrays into lists of ones-and-zeros"
+  [bytes] (map #(Integer/toBinaryString %) bytes))
+
+
+;; TODO:
+
+;; 1. Base64 algorithm:
+;;   a. transform seq of 8-bit bytes into seq of 6-bit patterns
+;;   b. look up decimal representation of 6-bit pattern
+;;   c. use this decimal as the index in the base64 table
+;;   d. repeat the above until we run out of 6-bit seqs
+;; 2. Swap 'unsafe' hex-to-bytes for a safer hex lookup (dict?)
+
+
+;; Step 2: encode the byte array in Base64
+;; (defn hex-to-base64
+;;   "Challenge 1: Convert hex to base64"
+;;   [hex] (let [bytes (hex-to-bytes hex)]) bytes)
--- test/cryptopals/set1_test.clj
+++ test/cryptopals/set1_test.clj
@@ -2,5 +2,15 @@
   (:require [clojure.test :refer :all]
             [cryptopals.set1 :as s]))
 
-(deftest hex-to-base64
-    (is (= "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t" (s/hex-to-base64 "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"))))
+(deftest hex-to-bytes
+  (is (= (s/hex-to-bytes "4d616e")
+         (map byte [0x4d 0x61 0x6e])
+         (map byte [77 97 110])
+         (map byte [2r1001101 2r1100001 2r1101110]))))
+
+(deftest print-bytes
+  (is (= (s/print-bytes (s/hex-to-bytes "4d616e"))
+         ["1001101" "1100001" "1101110"])))
+
+;; (deftest hex-to-base64
+;;     (is (= "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t" (s/hex-to-base64 "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"))))
